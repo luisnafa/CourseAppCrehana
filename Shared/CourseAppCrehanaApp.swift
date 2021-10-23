4cba75889a7a766529e9a628ff31605a9fb1ed4f
Luis Nafarrate <luis.nafarrate@syngenta.com>
2021-10-22 19:29:32 -0500

    Initial Commit

---
+++ Shared/CourseAppCrehanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppCrehanaApp.swift
+//  Shared
+//
+//  Created by Nafarrate, Luis on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppCrehanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
